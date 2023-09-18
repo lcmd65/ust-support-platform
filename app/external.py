97887e6fde3e29fc9affc30ffaa7e3cb2a8de178
Dat <dat.lemindast@gmail.com>
2023-09-18 16:34:51 +0700

    change app

--- app/external.py
+++ app/external.py
@@ -1,1 +1,2 @@
 app
+word2vec_model
--- app/main.py
+++ app/main.py
@@ -4,17 +4,16 @@ from flask import Flask, render_template, abort, request, jsonify
 from flask import request, redirect, url_for
 import codecs
 import gensim
+import external
 from distutils.version import LooseVersion, StrictVersion
 from packaging import version
 
 
-app = Flask(__name__)
+external.app = Flask(__name__)
+external.word2vec_model
 
 
-global word2vec_model
-
-
-@app.route('/search', methods=['GET', 'POST'])
+@external.app.route('/search', methods=['GET', 'POST'])
 def search():
     output = []
     if request.method == "POST":
@@ -24,8 +23,8 @@ def search():
         query = str(query).lower()
         output = []
         try:
-            sim_list = word2vec_model.most_similar(query, topn=50)
-            #output = word2vec_model.most_similar('u' + '\"' + query + '\"', topn=5)
+            sim_list = external.word2vec_model.most_similar(query, topn=50)
+            #output = external.word2vec_model.most_similar('u' + '\"' + query + '\"', topn=5)
             for wordsimilar in sim_list:
                 # output[wordsimilar[0]] = wordsimilar[1]
                 output.append(wordsimilar[0] + ' - '+ str(wordsimilar[1]))
@@ -36,12 +35,12 @@ def search():
             output = 'Not found' + query
     return render_template('search.html', pages = output)
 
-@app.route('/viewsearch', methods = ['GET', 'POST'])
+@external.app.route('/viewsearch', methods = ['GET', 'POST'])
 def viewanswer():
     
 
 
-@app.route("/home", methods = ['GETS', 'POST'] )
+@external.app.route("/home", methods = ['GETS', 'POST'] )
 def get_index():
     try:
         
@@ -59,10 +58,10 @@ if __name__ == "__main__":
         from packaging import version
         if version.parse(gensim.__version__) >= version.parse("1.0.1"):
             from gensim.models import KeyedVectors
-            word2vec_model = KeyedVectors.load_word2vec_format(model, binary=True)
+            external.word2vec_model = KeyedVectors.load_word2vec_format(model, binary=True)
         else:
             from gensim.models import Word2Vec
-            word2vec_model = Word2Vec.load_word2vec_format(model, binary=True)
-        app.run(port=8089)
+            external.word2vec_model = Word2Vec.load_word2vec_format(model, binary=True)
+        external.app.run(port=8089)
     else:
         print("Error")
