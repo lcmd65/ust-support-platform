d890b65b406b42d590e9ce43bf9c81371c975921
Dat <dat.lemindast@gmail.com>
2023-10-06 15:16:35 +0700

    change

--- test/test.py
+++ test/test.py
@@ -1,21 +1,2 @@
-from app.api.openai import api_getting
-import openai
-
-def main():
-    openai.api_key = api_getting()
-    message = "hey"
-    try:
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content": message}
-            ]
-        )
-        return completion['choices'][0]['message']['content']
-    except Exception as e:
-        # Handle the exception gracefully
-        return f"Failed to generate response: {e}"
-    
-if __name__ =="__main__":
-    print(main())
-
+import selenium
+from selenium import webdriver
