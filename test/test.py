05f4f929bebe4a790ff37c049aa04fc1d8dabf27
Dat <dat.lemindast@gmail.com>
2023-10-06 17:30:40 +0700

    change

--- test/test.py
+++ test/test.py
@@ -1,2 +1,15 @@
 import selenium
 from selenium import webdriver
+
+class Test():
+    def __init__(self):
+        self.driver = None
+        self.test = []
+        
+    def login_test(self):
+        self.driver.find("div", id = "login")
+        self.driver.
+    
+    def create_test(self):
+        pass
+        
